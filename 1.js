5f37e7b7db4d8684ce225ceaabbea533a0aa1b46
Seri8 <101550021+Seri8@users.noreply.github.com>
2025-10-02 05:15:43 +0330

    Delete 1.js

--- 1.js
+++
@@ -1,5 +0,0 @@
-// Renderer
-renderer = new THREE.WebGLRenderer({antialias:true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x001122); // changed from black (0x000000) to blue
-document.body.appendChild(renderer.domElement);
